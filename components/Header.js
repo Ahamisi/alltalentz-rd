d00f5928b1911533eed17a6add0078cad3e842c6
Ahamisi Godsfavour <ahamisi@Ahamisis-MacBook-Pro.local>
2023-08-15 18:20:29 +0100

    fixed gradient

--- components/Header.js
+++ components/Header.js
@@ -17,7 +17,7 @@ const Header = ({active='home'}) => {
 
 
   return (
-    <header className='md:w-[95%] md:ml-auto bg-gradient-to-b from-transparent to-black'>
+    <header className='md:w-[95%] md:ml-auto bg-gradient-to-b from-black to-black'>
         <nav className="py-4 px-8 flex items-center justify-between bg-transparent" style={{ zIndex: 10 }} >
       {/* Logo */}
       <div className="text-white text-2xl font-bold cursor-pointer items-center" >
