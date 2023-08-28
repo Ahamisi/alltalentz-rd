b5ccd19cc825c79546e5ff7c883a8b626fac2806
Ahamisi Godsfavour <ahamisi@Ahamisis-MacBook-Pro.local>
2023-08-28 13:37:43 +0100

    alltalents faq build

--- app/api/substack/route.js
+++ app/api/substack/route.js
@@ -4,30 +4,30 @@ import { NextRequest, NextResponse } from "next/server";
 
 
 
-export async function GET(req, res) {
+// export async function GET(req, res) {
 
-  if (req.method === 'GET') {
-    try {
-      const parser = new Parser();
-      const feed = await parser.parseURL('https://alltalentz.substack.com/feed');
+//   if (req.method === 'GET') {
+//     try {
+//       const parser = new Parser();
+//       const feed = await parser.parseURL('https://alltalentz.substack.com/feed');
 
-      const postsWithImages = await Promise.all(
-        feed.items.map(async (item) => {
-          const { content } = await parser.parseString(item['content:encoded']);
-          const $ = cheerio.load(content);
-          const firstImage = $('img').attr('src');
-          return { ...item, firstImage };
-        })
-      );
-      console.log(postsWithImages)
+//       const postsWithImages = await Promise.all(
+//         feed.items.map(async (item) => {
+//           const { content } = await parser.parseString(item['content:encoded']);
+//           const $ = cheerio.load(content);
+//           const firstImage = $('img').attr('src');
+//           return { ...item, firstImage };
+//         })
+//       );
+//       // console.log(postsWithImages)
 
-    //   return res.status(200).json(postsWithImages);
-    } catch (error) {
-      console.error('Error fetching RSS feed:', error);
-    //   return res.status(500).json({ message: 'An error occurred while fetching the RSS feed' });
-    }
-  } else {
-    console.log('Method not allowed')
-    // return res.status(405).json({ message: 'Method Not Allowed' });
-  }
-}
+//       return res.status(200).json(postsWithImages);
+//     } catch (error) {
+//       // console.error('Error fetching RSS feed:', error);
+//       return res.status(500).json({ message: 'An error occurred while fetching the RSS feed' });
+//     }
+//   } else {
+//     // console.log('Method not allowed')
+//     return res.status(405).json({ message: 'Method Not Allowed' });
+//   }
+// }
