3a41ce44792ecf20c4bb72dcdc548461e8088409
Ahamisi Godsfavour <ahamisivickzyjr@gmail.com>
2023-09-20 07:36:06 +0100

    reduced image sizes

--- components/Offerings.js
+++ components/Offerings.js
@@ -8,7 +8,7 @@ const Offerings = () => {
         <div className="max-w-6xl mx-auto grid grid-cols-2 gap-8 md:grid-cols-2">
             <div className="flex flex-row items-center gap-2 md:gap-[38px]">
               <div>
-                <Image src="/offerings/estimators.svg" height="78" width="78"/>
+                <Image src="/offerings/estimate.png" height="78" width="78"/>
               </div>
               <h3 className="text-m md:text-m md:text-xl font-semibold">Estimators</h3>
             </div>
@@ -23,14 +23,14 @@ const Offerings = () => {
   
             <div className="flex flex-row items-center gap-2 md:gap-[38px]">
               <div>
-                <Image src="/offerings/v-assistants.svg" height="78" width="78"/>
+                <Image src="/offerings/v-assistants.png" height="78" width="78"/>
               </div>
               <h3 className="text-m md:text-xl font-semibold">Virtual Assistants</h3>
             </div>
   
             <div className="flex flex-row items-center gap-2 md:gap-[38px]">
             <div>
-                <Image src="/offerings/tele-assistants.svg" height="78" width="78"/>
+                <Image src="/offerings/tele-assistants.png" height="78" width="78"/>
               </div>
               <h3 className="text-m md:text-xl font-semibold">Telemarketing Assistants</h3>
             </div>
@@ -39,7 +39,7 @@ const Offerings = () => {
   
             <div className="flex flex-row items-center gap-2 md:gap-[38px]">
             <div>
-                <Image src="/offerings/sm-strategists.svg" height="78" width="78"/>
+                <Image src="/offerings/sm-strategists.png" height="78" width="78"/>
               </div>
               <h3 className="text-m md:text-xl font-semibold">Digital Marketers / Social Media Strategists</h3>
             </div>
@@ -49,7 +49,7 @@ const Offerings = () => {
   
             <div className="flex flex-row items-center gap-2 md:gap-[38px]">
             <div>
-                <Image src="/offerings/designers-devs.svg" height="78" width="78"/>
+                <Image src="/offerings/designers-dev.png" height="78" width="78"/>
               </div>
               <h3 className="text-m md:text-xl font-semibold">Designers / Software Developers</h3>
             </div>
@@ -58,7 +58,7 @@ const Offerings = () => {
   
             <div className="flex flex-row items-center gap-2 md:gap-[38px]">
             <div>
-                <Image src="/offerings/a-recievables.svg" height="78" width="78"/>
+                <Image src="/offerings/account-receivables.png" height="78" width="78"/>
               </div>
               <h3 className="text-m md:text-xl font-semibold">Account Receivables</h3>
             </div>
@@ -67,7 +67,7 @@ const Offerings = () => {
   
             <div className="flex flex-row items-center gap-2 md:gap-[38px]">
             <div>
-                <Image src="/offerings/qb-specialists.svg" height="78" width="78"/>
+                <Image src="/offerings/qb-specialists.png" height="78" width="78"/>
               </div>
               <h3 className="text-m md:text-xl font-semibold">Quick book Specialists</h3>
             </div>
