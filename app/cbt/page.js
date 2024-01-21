81f82ed53073a3f5b77d3ee79b19690afdfb82bc
Ahamisi Godsfavour <ahamisivickzyjr@gmail.com>
2024-01-21 12:48:16 +0100

    60 Minutes

--- app/cbt/page.js
+++ app/cbt/page.js
@@ -6,7 +6,7 @@ import { useRouter } from "next/navigation";
 const CBT = () => {
   const router = useRouter();
 
-  const totalDuration = 40 * 60 * 1000;
+  const totalDuration = 60 * 60 * 1000;
   const redThreshold = 1 * 60 * 1000;
 
   const [remainingTime, setRemainingTime] = useState(totalDuration);
