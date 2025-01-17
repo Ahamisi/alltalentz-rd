7e91169d380f34dca55d25f6d8c20ed1d03ffe96
ahamisi <ahamisivickzyjr@gmail.com>
2025-01-17 15:45:54 -0800

    Fixed Bg black isssue that was showing a s gray

--- components/PageHeader.js
+++ components/PageHeader.js
@@ -4,7 +4,7 @@ const PageHeader = ({ children, about = false, bgImage, showBg = true }) => {
 
   return (
     <section
-      className={`relative ${about ? 'h-auto lg:h-screen pb-12 lg:pb-0' : 'h-auto pb-12'} ${bgImage ? 'pb-3' : ''}  ${showBg? 'bg-black' : ''} bg-cover bg-center bg-no-repeat mt-[0px] px-[20px] md:px-0 lg:px-0`}
+      className={`relative ${about ? 'h-auto lg:h-screen pb-12 lg:pb-0' : 'h-auto pb-12'} ${bgImage ? 'pb-3' : ''}  ${showBg? '' : 'bg-black'} bg-cover bg-center bg-no-repeat mt-[0px] px-[20px] md:px-0 lg:px-0`}
       style={{ backgroundImage: showBg ? `url(${bgImage || (about ? '/home-bg.jpg' : '/all-talentz-header.jpg')})` : 'none' }}
     >
       <Header theme={bgImage ? 'light' : 'dark'} />
