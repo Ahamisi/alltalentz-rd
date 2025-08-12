748fe3cd07e3e3bd90b8f65e57c989191276fe77
Godsfavour Ahamisi <theaceman@Godsfavours-MacBook-Pro.local>
2025-08-13 00:36:23 +0100

    Cobvert Actual Bootcamp Page

--- app/bootcamp/page.js
+++ app/bootcamp/page.js
@@ -23,7 +23,7 @@ export default function BootCamp() {
   
 
   const [isDuplicate, setIsDuplicate] = useState(null);
-  const [bootCampOver, setBootCampOver] = useState(true);
+  const [bootCampOver, setBootCampOver] = useState(false);
 
 
 
@@ -200,7 +200,7 @@ export default function BootCamp() {
             if (prevCountdown === 0) {
               clearInterval(countdownInterval);
               console.log("done");
-              route.push("https://forms.office.com/r/efisy50hkD?origin=lprLink");
+              route.push("https://forms.cloud.microsoft/pages/responsepage.aspx?id=fhgFiTIHCkquTYO-BRMRBfQf36Oi-MZHkm_eYMKcGU9UMlQwS1pYR0xHSzlCOUpFSjU3RDlWRVZXTC4u");
               // route.push("https://forms.office.com/pages/responsepage.aspx?id=fhgFiTIHCkquTYO-BRMRBfQf36Oi-MZHkm_eYMKcGU9UMTkwQ1RXVDFOVzBIVk5US0g5T0NBM0NBWi4u&route=shorturl")
               // route.push("https://alltalentz.com/cbt");
               return prevCountdown; // Countdown should not change when it reaches 0
@@ -426,7 +426,7 @@ export default function BootCamp() {
                   </div>
                   <h3 className="text-xl font-semibold mb-2 text-black">Thank you!</h3>
                   <p className="text-gray-600">Wait while we redirect you to the test portal in {countdown} seconds.</p><br/>
-                  <Btn link="https://forms.office.com/r/efisy50hkD?origin=lprLink" target="_blank" text="Take Test Now" className="mt-6" />
+                  <Btn link="https://forms.cloud.microsoft/pages/responsepage.aspx?id=fhgFiTIHCkquTYO-BRMRBfQf36Oi-MZHkm_eYMKcGU9UMlQwS1pYR0xHSzlCOUpFSjU3RDlWRVZXTC4u" target="_blank" text="Take Test Now" className="mt-6" />
 
                   {/* <Btn link="https://alltalentz.com/cbt" target="_blank" text="Take Test Now" className="mt-6" /> */}
                   
@@ -439,8 +439,14 @@ export default function BootCamp() {
                   </div>
               ) : (
                 <form onSubmit={handleSubmit} encType="multipart/form-data" method="post" className="text-[#A6A6A6]">
-                <h2 className="text-lg font-normal text-center mb-8 text-[#939393]">Kindly fill this form and upload your CV to keep yourself in the loop</h2>
+                {/* <h2 className="text-lg font-normal text-center mb-8 text-[#939393]">Kindly fill this form and upload your CV to keep yourself in the loop</h2> */}
+                <h2 className="text-lg font-normal text-center mb-8 text-[#939393]">
+                        🇬🇭 <span className="ml-1">Strictly for Candidates from Ghana</span>
+                      </h2>
           
+          
+
+
                 <div className="mb-8">
                   <input
                     type="text"
@@ -526,7 +532,7 @@ export default function BootCamp() {
                 </div>
           
                 <div className="mb-8">
-                  <label className="block text-gray-700 mb-1">Upload NYSC Cert.</label>
+                  <label className="block text-gray-700 mb-1">Upload NSS (National Service Scheme) Cert.</label>
                   <input
                     type="file"
                     name="nysc"
@@ -539,7 +545,11 @@ export default function BootCamp() {
                   )}
                   <div className="flex items-center bg-blue-500 text-white text-sm font-bold px-4 py-3 mt-3" role="alert">
                     <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.432 0c1.34 0 2.01.912 2.01 1.957 0 1.305-1.164 2.512-2.679 2.512-1.269 0-2.009-.75-1.974-1.99C9.789 1.436 10.67 0 12.432 0zM8.309 20c-1.058 0-1.833-.652-1.093-3.524l1.214-5.092c.211-.814.246-1.141 0-1.141-.317 0-1.689.562-2.502 1.117l-.528-.88c2.572-2.186 5.531-3.467 6.801-3.467 1.057 0 1.233 1.273.705 3.23l-1.391 5.352c-.246.945-.141 1.271.106 1.271.317 0 1.357-.392 2.379-1.207l.6.814C12.098 19.02 9.365 20 8.309 20z"/></svg>
-                    <p>Hey there, once you click submit, you would be redirected to take a compulsory test as the final stage.</p>
+                    <p>Hey there, once you click submit, you would be redirected to take a compulsory test as the final stage. 
+                      <span className="inline-flex items-center ml-2">
+                        🇬🇭 <span className="ml-1">Strictly for Candidates from Ghana</span>
+                      </span>
+                    </p>
                   </div>
                 </div>
           
