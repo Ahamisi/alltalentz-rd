9e074753d74f9ee8a9a906d2cdea7de6c9b5f973
Oluwatoyin <105533960+Shortiii@users.noreply.github.com>
2026-02-20 14:23:00 +0100

    hero images fixed

--- app/services/page.js
+++ app/services/page.js
@@ -7,10 +7,10 @@ export default function Services() {
   return (
     <main className="relative overflow-hidden overflow-y-hidden ">
       <PageHeader>
-        <div className="max-w-7xl mx-auto px-4 lg:flex h-[100%] items-center ">
+        <div className="max-w-7xl mx-auto px-4 lg:flex mt-0 lg:mt-12 h-[100%] items-center ">
           {/* Left Column */}
           <div className="lg:w-[55%] flex flex-col space-y-8 py-20">
-            <h1 className="text-4xl md:text-6xl font-bold text-white leading-tight">
+            <h1 className="text-4xl md:text-6xl font-bold text-white leading-snug">
               Your Strategic Partner for <span className="text-[#FFB300]">Industry-Specific Talents</span>
             </h1>
             
@@ -25,7 +25,7 @@ export default function Services() {
           {/* Right Column - Map */}
           <div className="hidden lg:block lg:w-[45%] absolute bottom-[-1px] right-4 pl-0">
             <img
-                    src="/redesign-25/newservice-header.png"
+                    src="/redesign-25/service-header.png"
                     alt="Success Stories"
                     className="w-full h-[500px] object-contain"
                 />
