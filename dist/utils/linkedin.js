b1ca721174a2b9b0ed0a151ce75b61eeb9e40d8d
whizzie99 <wisdomobiomadev@gmail.com>
2026-04-22 13:52:59 +0100

    feat: implement blog page

---
+++ dist/utils/linkedin.js
@@ -0,0 +1,13 @@
+
+(function(l) {
+if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+window.lintrk.q=[]}
+var s = document.getElementsByTagName("script")[0];
+var b = document.createElement("script");
+b.type = "text/javascript";b.async = true;
+b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+s.parentNode.insertBefore(b, s);})(window.lintrk);
+
+// <noscript id='linkd3'>
+// <img height="1" width="1" style="display:none;" alt="" src="https://px.ads.linkedin.com/collect/?pid=4798922&fmt=gif" />
+// </noscript>
--- sanity.cli.ts
+++ sanity.cli.ts
@@ -5,4 +5,7 @@ export default defineCliConfig({
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET ?? 'production',
   },
+  deployment: {
+    appId: 'zo0s0wiehywi2tshnvhcj4bo',
+  },
 })
