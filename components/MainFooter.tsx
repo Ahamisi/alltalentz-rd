3dda635ff3c2d85814769b0bf6918bfba40c1877
Godsfavour Ahamisi <theaceman@Godsfavours-MacBook-Pro.local>
2026-05-30 11:00:47 +0100

    restoring Excellence Logo

--- components/MainFooter.tsx
+++ components/MainFooter.tsx
@@ -4,7 +4,7 @@ import SubFooter from "./SubFooter";
 import Link from "next/link";
 import Btn from "./Btn";
 import Image from "next/image";
-import LogoTagline from "./LogoTagline";
+import AllTalentzLogo from "./AllTalentzLogo";
 
 import { motion } from "framer-motion";
 
@@ -197,10 +197,7 @@ const MainFooter = ({
           {/* third Row */}
           <div className="flex flex-col md:flex-row justify-between items-center gap-8">
             <div className="flex flex-col md:flex-row items-center gap-8 flex-1">
-              <div className="flex flex-col items-start">
-                <Image src="/all-talents-footer.svg" alt="Footer Logo" width={150} height={40} />
-                <LogoTagline />
-              </div>
+              <AllTalentzLogo className="w-[150px] md:w-[180px] h-auto" />
               <div className="text-white text-center md:text-left text-sm">
                 © {new Date().getFullYear()} All Talentz LLC. All rights reserved.
               </div>
